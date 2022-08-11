d5212f52cd41d0a416a01d5b1ea99140559b5269
Męczyciel Biedronek <44778546+MeczycielBiedronek@users.noreply.github.com>
2022-08-11 12:55:05 +0200

    feat: add mkdir

--- app.js
+++ app.js
@@ -6,31 +6,40 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-
-
+            ///// UPLOAD FILE 
 const storage = multer.diskStorage({
 
-    destination: function(req, file, cb) {
+  destination: (req, file, cb) => {
+    // Generates new date (y m d)
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+    let newdate = "_" + year + month + day;
 
-        cb(null, 'public/uploads/');
-
-    },
+    const dest = "public/uploads@a/" + req.body.email + newdate;
+    fs.access(dest, function (error) {
+      if (error) {
+        console.log("Directory created");
+        return fs.mkdir(dest, (error) => cb(error, dest));
+      } else {
+        console.log("Directory exists.");
+        return cb(null, dest);
+      }
+    });
+  },
 
   
 
     filename: function(req, file, cb) {
         console.log(file.originalname);
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, file.originalname);
       
     }
 
 });
 
-  
-
 const upload = multer({ storage: storage })
-
-  
 
 app.get('/', (req, res) => {
 
@@ -41,13 +50,17 @@ app.get('/', (req, res) => {
   
 
 app.post('/', upload.array('multi-files'), (req, res) => {
+
+  // upload progress desktop - simulation
+  console.log('file uploaded');
   const myTimeout = setTimeout(function(){
     res.redirect('/');
-}, 5000);
+}, 3000);
+
  
 
 });
-
+////////////  SEND EMAIL
   
 
 app.listen(80);
